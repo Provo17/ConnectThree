f6394cd831b507a229fc49ddabd799677259f1c0
Provo17 <provo17.gp@gmail.com>
2017-12-13 21:58:24 -0600

    Added elements for UI.

--- app/src/main/java/com/example/gregory/connectthree/MainActivity.java
+++ app/src/main/java/com/example/gregory/connectthree/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gregory.connectthree;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     // 2 means empty space on game
 
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    int [][] winningPositions = {{0,1,2}, {3, 4, 5}, {6, 7, 8},
+                                 {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+                                 {0, 4, 8}, {2, 4, 6}};
 
     public void dropIn (View view) {
 
@@ -43,6 +48,16 @@ public class MainActivity extends AppCompatActivity {
 
             }
             counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+
+            for (int [] winningPosition : winningPositions) {
+
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                        gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                        gameState[winningPosition[0]] != 2){
+                    System.out.println(gameState[winningPosition[0]]);
+                }
+
+            }
         }
 
 
